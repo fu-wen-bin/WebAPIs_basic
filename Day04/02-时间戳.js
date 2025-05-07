d458dcfd06ea8d45b4da7b30950ad4240b2df98d
文斌 傅 <fwb2580@outlook.com>
2025-05-07 23:09:09 +0800

    改改了代码缩进，保持代码格式统一

--- Day04/02-时间戳.js
+++ Day04/02-时间戳.js
@@ -1,12 +1,12 @@
 //时间戳
-  // 1.getTime() --必须要实例化 要有new
-  const date=new Date();
-  console.log(date.getTime())
+// 1.getTime() --必须要实例化 要有new
+const date = new Date()
+console.log(date.getTime())
 
-  // 2. +new Date()
-  console.log(+new Date())
-  // 获取指定时间戳
-  console.log(+new Date('2025-4-26 15:46:00'))
+// 2. +new Date()
+console.log(+new Date())
+// 获取指定时间戳
+console.log(+new Date('2025-4-26 15:46:00'))
 
-  // 3. Date.now()
-  console.log(Date.now())
+// 3. Date.now()
+console.log(Date.now())
